e39315093a2adffb24e6803afcd8dd9e3f9df9d9
Joel Estrada <joeljazzhands@Joels-MBP.attlocal.net>
2023-03-14 17:32:41 -0700

    Code runs good I put it all together and tested it out, shouldve added more commits but I focused on making sure itll ass work

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,173 @@
+var enter;
+var numberConfirm;
+var characterConfirm;
+var upperConfirm;
+var lowerConfirm;
+
+var numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var upperCasedCharacters = ['A',
+'B',
+'C',
+'D',
+'E',
+'F',
+'G',
+'H',
+'I',
+'J',
+'K',
+'L',
+'M',
+'N',
+'O',
+'P',
+'Q',
+'R',
+'S',
+'T',
+'U',
+'V',
+'W',
+'X',
+'Y',
+'Z'];
+var lowerCasedLetter = ['a',
+'b',
+'c',
+'d',
+'e',
+'f',
+'g',
+'h',
+'i',
+'j',
+'k',
+'l',
+'m',
+'n',
+'o',
+'p',
+'q',
+'r',
+'s',
+'t',
+'u',
+'v',
+'w',
+'x',
+'y',
+'z',
+];
+var specialCharacters = [
+      '@',
+      '%',
+      '+',
+      '\\',
+      '/',
+      "'",
+      '!',
+      '#',
+      '$',
+      '^',
+      '?',
+      ':',
+      ',',
+      ')',
+      '(',
+      '}',
+      '{',
+      ']',
+      '[',
+      '~',
+      '-',
+      '_',
+      '.',
+    ];
+space = [];
+var choices;
+
+function generatePassword(){
+  enter = parseInt(window.prompt('Password Length between 8 and 128'));
+  if (!enter){
+    window.alert("It needs a value")
+  }
+  else if (enter < 8 || enter > 128){
+    enter = window.prompt("must be between 8 and 128")
+  } else {
+    numberConfirm = window.confirm("Would you like numbers in your password?")
+    characterConfirm = window.confirm("Would you like characters in your password?")
+    upperConfirm = window.confirm("Would ypou like capitilized letters in your password?")
+    lowerConfirm = window.confirm("Would you like lower case letters in your password?")
+  }
+  if (!numberConfirm && !upperConfirm && !characterConfirm && !lowerConfirm){
+    choices = wimdow.alert("you must choose atleast 1")
+  } 
+  else if (numberConfirm && characterConfirm && upperConfirm && lowerConfirm){
+    choices = specialCharacters.concat(numberArr,upperCasedCharacters,lowerCasedLetter)
+  }
+  else if (characterConfirm && numberConfirm && upperConfirm){
+    choices = specialCharacters.concat(upperCasedCharacters, numberConfirm)
+  }
+  else if (characterConfirm && numberConfirm && lowerConfirm){
+    choices = specialCharacters.concat(numberArr, lowerCasedLetter)
+  }
+  else if (characterConfirm && upperConfirm && lowerConfirm){
+    choices = specialCharacters.concat(upperCasedCharacters, lowerCasedLetter)
+  }
+  else if (upperConfirm && lowerConfirm && numberConfirm){
+    choices = numberArr.concat(lowerCasedLetter, upperCasedCharacters)
+  }
+  else if (upperConfirm && lowerConfirm){
+    choices = upperCasedCharacters.concat(lowerCasedLetter)
+  }
+  else if (upperConfirm && numberConfirm){
+    choices = upperCasedCharacters.concat(numberArr)
+  }
+  else if (upperConfirm && characterConfirm){
+    choices = upperCasedCharacters.concat(specialCharacters)
+  }
+  else if (lowerConfirm && characterConfirm){
+    choices = lowerCasedLetter.concat(specialCharacters)
+  }
+  else if (lowerConfirm && numberConfirm){
+    choices = lowerCasedLetter.concat(numberArr)
+  }
+  else if (numberConfirm && characterConfirm){
+    choices = numberArr.concat(specialCharacters)
+  }
+  else if (numberConfirm){
+    choices = numberArr
+  }
+  else if (characterConfirm){
+    choices = specialCharacters
+  }
+  else if (upperConfirm){
+    choices = upperCasedCharacters
+  }
+  else if (lowerConfirm){
+    choices = space.concat(lowerCasedLetter)
+  }
+  var password = [];
+  for (i = 0; i < enter; i++){
+  var pickChoices = choices[Math.floor(Math.random() * choices.length)]
+  password.push(pickChoices)
+}
+  var ps = password.join("")
+  writePassword(ps);
+  return ps
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function writePassword(ps) {
+    document.querySelector("#password").textContent = ps
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// // Add event listener to generate button
+generateBtn.addEventListener("click", function(){
+  ps = generatePassword()
+});
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # password-simulator
+
+## Description
+    I found this project really challenging at first, I went at it all wrong and put myself through unnecessary stress when the answer was so simple.
+    I was console logging it all and figured i was doing it right but others example in the class made me realize how wrong I really was. I put every option into a function when all I had to do was put alot of else if statements like it was the rock paper scissors game. I was also not putting my variable globally at first I kept putting it locally which caused ton of problems. Always a good thing to look back at old classroom recording and always good to take advice from others and look at others work online to and see how they managed to execute their code.
+
+## Usage
+    This will password generator will help you generate a strong password based off of what letters characters and numbers you want all you have to do is click generate password button and answer the questions that the window pops up to see if you want certain objects in your password then itll generate one for you.
+
+
+![password-simulator](imgs/picture.png)
+
+![password-simulator](imgs/picture2.html.png)
+
+## Link
+
+## License
+Please refer to license in the repo
